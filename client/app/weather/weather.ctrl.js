3a3dc91b6a9d697640c7f8a15343308ad2de1df8
Hensley, Michael (DS) <Michael.Hensley@cdk.com>
2015-05-17 00:49:08 -0700

    First attempt at the dashboard! Angular is really awesome

--- client/app/weather/weather.ctrl.js
+++ client/app/weather/weather.ctrl.js
@@ -2,12 +2,40 @@
 
 angular.module('weatherDemoApp')
   .controller('WeatherCtrl', function ($http) {
-    var vm = this;
-    vm.weatherArray = [];
-    $http.get('/api/weather/la').success(function(data) {
-        for(var i = 0; i < 8; i++){
-            vm.weatherArray.push(data);
-        }
-    });
 
+    var vm = this;
+
+        /**
+         *
+         * @type {Array}
+         * Array to hold API Results on Scope
+         */
+    vm.weatherArray = [];
+    var API_ENDPOINT = '/api/weather';
+
+        /***
+         *
+         * key/value pairs for weather API endpoints
+         */
+    var cityEndpoints = {
+        la:  '/la',
+        snf: '/snf',
+        ny:  '/ny',
+        bos: '/bos',
+        por: '/por',
+        sea: '/sea',
+        hou: '/hou',
+        chi: '/chi'
+    };
+
+    function initializeWeatherData(){
+        angular.forEach(cityEndpoints, function(cityEndpoint, key){
+            $http.get(API_ENDPOINT + cityEndpoint)
+                .success(function(data){
+                    vm.weatherArray.push(data);
+                })
+        });
+    }
+
+        initializeWeatherData();
   });
