e3315c166a07424acde554e5a495c96198cda1e2
Hensley, Michael (DS) <Michael.Hensley@cdk.com>
2015-05-17 22:56:52 -0700

    changing the order of some controller variables

--- client/app/weather/weather.ctrl.js
+++ client/app/weather/weather.ctrl.js
@@ -1,41 +1,32 @@
 'use strict';
 
 angular.module('weatherDemoApp')
-  .controller('WeatherCtrl', function ($http) {
+    .controller('WeatherCtrl', function ($http) {
+        var API_ENDPOINT = '/api/weather';
+        var CITY_ENDPOINTS = {
+            la: '/la',
+            snf: '/snf',
+            ny: '/ny',
+            bos: '/bos',
+            por: '/por',
+            sea: '/sea',
+            hou: '/hou',
+            chi: '/chi'
+        };
 
-    var vm = this;
+        var vm = this;
+        vm.weatherArray = [];
 
-        /**
-         *
-         * @type {Array}
-         * Array to hold API Results on Scope
-         */
-    vm.weatherArray = [];
-    var API_ENDPOINT = '/api/weather';
-
-        /***
-         *
-         * key/value pairs for weather API endpoints
-         */
-    var cityEndpoints = {
-        la:  '/la',
-        snf: '/snf',
-        ny:  '/ny',
-        bos: '/bos',
-        por: '/por',
-        sea: '/sea',
-        hou: '/hou',
-        chi: '/chi'
-    };
-
-    function initializeWeatherData(){
-        angular.forEach(cityEndpoints, function(cityEndpoint, key){
-            $http.get(API_ENDPOINT + cityEndpoint)
-                .success(function(data){
-                    vm.weatherArray.push(data);
-                })
-        });
-    }
+        function initializeWeatherData() {
+            angular.forEach(CITY_ENDPOINTS, function (cityEndPoint) {
+                $http.get(API_ENDPOINT + cityEndPoint)
+                    .success(function (data) {
+                        vm.weatherArray.push(data);
+                    })
+            });
+        }
 
         initializeWeatherData();
-  });
+    });
+
+
